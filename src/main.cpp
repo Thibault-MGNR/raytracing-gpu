cc751da6c990b15ab5fd7493c91ee3b1d8a2e71b
Thibault-MGNR <titi76mag@gmail.com>
2024-07-13 22:27:12 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <GPGPU/GPGPU.hpp>
+#include <filesystem>
+
+class App: public GPGPU {
+	public:
+		void init(){
+			setRenderView({1000.f, 1000.f}, 2);
+
+			initGPGPU();
+			_cs.init("C:/Users/Thibault/Documents/programmation/raytracing/lib/GPGPU/src/shaders/computeShader.glsl");
+		}
+
+		~App() {
+
+		}
+
+	private:
+		virtual void renderFrame() override {
+			_cs.use();
+			_cs.setFloat("t", _window.getTime());
+			glDispatchCompute((unsigned int)1000/8, (unsigned int)1000/8, 1);
+
+			glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+		}
+
+		ComputeShader _cs;
+};
+
+int main()
+{
+	App app;
+	app.init();
+
+	app.run();
+
+	return EXIT_SUCCESS;
+}
